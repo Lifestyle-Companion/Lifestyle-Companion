1413add2cc8faad651fc2c1abe2892f448cd35ac
Lifestyle-Companion <mlwest@live.com.au>
2026-08-17 20:12:14 +1000

    Add files via upload
    HEC Trial Alpha 0.6.30

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "healthy-eating-companion-alpha-0-6-29-v1";
-const VERSION = "0.6.29";
+const CACHE_NAME = "healthy-eating-companion-alpha-0-6-30-v1";
+const VERSION = "0.6.30";
 const CORE_FILES = [
   `./index.html`,
   `./styles.css?v=${VERSION}`,
